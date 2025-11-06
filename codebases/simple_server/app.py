34f15fa68ed17c2cce902f9077a2233fac40894c
Anna <89742951+AnyaVeselova@users.noreply.github.com>
2025-11-06 20:18:18 +0000

    Simplify home route response message

--- codebases/simple_server/app.py
+++ codebases/simple_server/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_home():
-    return "Hello, world! I am running on Flask with CI/CD!"
+    return "Hello, world!"
 
 if __name__ == '__main__':
     app.run(

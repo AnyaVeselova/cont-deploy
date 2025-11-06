60e5fb1785a45ed7f3692c98040d158f317cbef8
Anna Veselova <anna1995correspondence@gmail.com>
2025-11-06 18:45:39 +0000

    chore: updates text

--- codebases/simple_server/app.py
+++ codebases/simple_server/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_home():
-    return "Hello, world!"
+    return "Hello, world! I am running on Flask with CI/CD!"
 
 if __name__ == '__main__':
     app.run(
